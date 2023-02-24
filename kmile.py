a5c33d16206fd7ff97f5c52ce43e5287251b05ea
Alfie <zurimeg@gmail.com>
2023-02-24 04:10:33 +0400

    Add Convertation

--- kmile.py
+++ kmile.py
@@ -3,3 +3,11 @@ print("km  to mile cinverter")
 unit = input ("km or mile: ")
 amount = float(input("Amount: "))
 
+if unit == "km":
+  print (amount / 1.6)
+
+elif unit == "mile":
+  print (amount * 1.6)
+
+else:
+  print("Error"
